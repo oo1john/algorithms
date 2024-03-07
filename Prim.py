d1d1841e9d2c26e7975520b21a87113c9927a3e3
John <gv.ionut@gmail.com>
2024-03-07 10:51:01 +0200

    Create Prim.py

---
+++ Prim.py
@@ -0,0 +1,29 @@
+import heapq
+
+def prim(graph, start):
+    mst = []
+    visited = set([start])
+    edges = [(cost, start, to) for to, cost in graph[start].items()]
+    heapq.heapify(edges)
+
+    while edges:
+        cost, frm, to = heapq.heappop(edges)
+        if to not in visited:
+            visited.add(to)
+            mst.append((frm, to, cost))
+
+            for to_next, cost in graph[to].items():
+                if to_next not in visited:
+                    heapq.heappush(edges, (cost, to, to_next))
+
+    return mst
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': {'B': 2, 'C': 3},
+    'B': {'A': 2, 'C': 1, 'D': 1},
+    'C': {'A': 3, 'B': 1, 'D': 5},
+    'D': {'B': 1, 'C': 5}
+}
+
+print(prim(graph, 'A'))
