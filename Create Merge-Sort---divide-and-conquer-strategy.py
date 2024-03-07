bc04e54a5446de3e4d74528b91422183ff02c01c
John <gv.ionut@gmail.com>
2024-03-07 10:58:28 +0200

    Create Create Merge-Sort---divide-and-conquer-strategy.py

---
+++ Create Merge-Sort---divide-and-conquer-strategy.py
@@ -0,0 +1,28 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+    left = merge_sort(arr[:middle])
+    right = merge_sort(arr[middle:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    left_index = right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] < right[right_index]:
+            result.append(left[left_index])
+            left_index += 1
+        else:
+            result.append(right[right_index])
+            right_index += 1
+
+    result.extend(left[left_index:])
+    result.extend(right[right_index:])
+    return result
+
+# Example usage
+print(merge_sort([38, 27, 43, 3, 9, 82, 10]))
