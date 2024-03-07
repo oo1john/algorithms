27611999f1b70b970edc6a24ab6c42102c38b40f
John <gv.ionut@gmail.com>
2024-03-07 10:52:04 +0200

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,12 @@
+# Using Memoization
+
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        return 1
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
+
+# Example usage
+print(fibonacci(10))  # Outputs: 55
